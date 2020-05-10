324b443bf4dfdfdb0f1de14489f7c742acf4ba54
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2020-05-10 13:03:20 -0700

    added get method to tst

--- Ternary_Search_Tree.py
+++ Ternary_Search_Tree.py
@@ -23,7 +23,7 @@ class TST:
     if letter < node.character:
       node.leftNode = self.__putRec(word, index, node.leftNode, number)
     elif letter > node.character:
-      node.right = self.__putRec(word, index, node.rightNode, number)
+      node.rightNode = self.__putRec(word, index, node.rightNode, number)
     elif index < len(word) - 1:
       node.midNode = self.__putRec(word, index+1, node.midNode, number)
     else:
@@ -31,13 +31,38 @@ class TST:
 
     return node
 
+  def get(self, word):
+    node = self.__getRec(word, 0, self.root)
+
+    if not node:
+      return -1
+    
+    return node.value
+
+  def __getRec(self, word, index, node):
+
+    if not node:
+      return None
+    
+    letter = word[index]
+
+    if letter < node.character:
+      return self.__getRec(word, index, node.leftNode)
+    elif letter > node.character:
+      return self.__getRec(word, index, node.rightNode)
+    elif index < len(word) - 1:
+      return self.__getRec(word, index+1, node.midNode)
+    else:
+      return node
 
 myTree = TST()
 myTree.put('car', 23)
 myTree.put('cat', 40)
 myTree.put('bat', 19)
 
-print('done')
+print(myTree.get('cat'))
+
+
 
 
 
